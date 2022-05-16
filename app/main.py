bbe999e65d68f4e90c83ae29204af45889a3aed0
Pavel Druzhinin <paveldroo@gmail.com>
2022-05-16 18:31:30 +0300

    add 2 endpoints with sqlalchemy queries: all() and search by id

--- app/main.py
+++ app/main.py
@@ -18,9 +18,21 @@ def get_db():
 
 
 @app.post('/blog')
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get('/blog')
+def get_all_blogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{blog_id}')
+def get_blog_by_id(blog_id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+    return blog
