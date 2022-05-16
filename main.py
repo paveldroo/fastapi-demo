e71ff910686a9ee4d9748c86792d8bd51096d68c
Pavel Druzhinin <paveldroo@gmail.com>
2022-05-16 15:58:07 +0300

    add query params

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+from typing import Optional
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': 'blog list'}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'First {limit} published blog records from db'}
+    else:
+        return {'data': f'First {limit} blog records from db'}
 
 
 @app.get('/blog/unpublished')
@@ -19,5 +24,5 @@ def about(blog_id: int):
 
 
 @app.get('/blog/{blog_id}/comments')
-def comments(blog_id: int):
+def comments(blog_id: int, limit=10):
     return {'data': ['1', '2']}
