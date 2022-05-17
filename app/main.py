dbf1672155ac36e3dc6228cb87c12f3ed1630c80
Pavel Druzhinin <paveldroo@gmail.com>
2022-05-17 11:33:20 +0300

    Add response models

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
 
@@ -46,13 +48,13 @@ def update_blog(blog_id, request: schemas.Blog, db: Session = Depends(get_db)):
     return 'updated'
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog])
 def get_all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{blog_id}', status_code=200)
+@app.get('/blog/{blog_id}', status_code=200, response_model=schemas.ShowBlog)
 def get_blog_by_id(blog_id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
     if not blog:
