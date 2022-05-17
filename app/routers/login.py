2560ed131073e367e06c9ac1d26c2409014763c7
Pavel Druzhinin <paveldroo@gmail.com>
2022-05-17 19:18:49 +0300

    add jwt token generation on login endpoint

--- app/routers/login.py
+++ app/routers/login.py
@@ -5,6 +5,7 @@ from starlette import status
 from app import schemas, models
 from app.database import get_db
 from app.hashing import Hash
+from app.token import create_access_token
 
 router = APIRouter(
     tags=['authentication']
@@ -20,5 +21,5 @@ def login(request: schemas.Login, db: Session = Depends(get_db)):
     if not Hash.verify(user.password, request.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail='Incorrect password')
-
-    return user
+    access_token = create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
