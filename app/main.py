ba1d4e133e956e38c6abdcbd335604d9e3704aaf
Pavel Druzhinin <paveldroo@gmail.com>
2022-05-17 12:09:56 +0300

    Add get_user endpoint

--- app/main.py
+++ app/main.py
@@ -64,10 +64,19 @@ def get_blog_by_id(blog_id, db: Session = Depends(get_db)):
     return blog
 
 
-@app.post('/user')
+@app.post('/user', response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get('/user/{user_id}', response_model=schemas.ShowUser)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'User with the id {user_id} is not available')
+    return user
